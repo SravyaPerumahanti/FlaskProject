fc5d1f3250e01e623bfabdad27cb31f7f8b50b77
SravyaP <sravya.p1990@gmail.com>
2026-04-21 18:00:48 +0530

    modified app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Hello from Jenkins CI/CD!"
 
 if __name__ == "__main__":
-    app.run()
+    # 0.0.0.0 allows external access (important for Docker later)
+    app.run(host="0.0.0.0", port=5000)
