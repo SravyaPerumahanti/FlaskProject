0b84d86f9467522b5a563b909a2861c40b60f12b
SravyaP <sravya.p1990@gmail.com>
2026-04-22 13:46:10 +0530

    added version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD!"
+    return "Hello from version2!"
 
 if __name__ == "__main__":
     # 0.0.0.0 allows external access (important for Docker later)
